318cfec15323d5fb742c25284c6bd65d4c23a2ee
OmarAndresCuadrado <omarcuadradodev@gmail.com>
2021-08-09 13:40:53 -0600

    dist folder

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,50 +2,50 @@ export const environment = {
   production: false,
 
   webSocket: {
-    host: 'http://3.137.213.20:8085/websocket'
+    host: 'http://localhost:8080/websocket'
   },
 
   teacher: {
-    host: 'http://3.137.213.20:8085/api/teacher',
-    host_image: 'http://3.137.213.20:8085/teacher/upload/image'
+    host: 'http://localhost:8080/api/teacher',
+    host_image: 'http://localhost:8080/teacher/upload/image'
   },
 
   student: {
-    host: 'http://3.137.213.20:8085/api/student'
+    host: 'http://localhost:8080/api/student'
   },
 
   transaction: {
-    host: 'http://3.137.213.20:8085/api/payment'
+    host: 'http://localhost:8080/api/payment'
   },
 
   transfers: {
-    host: 'http://3.137.213.20:8085/api/transfer'
+    host: 'http://localhost:8080/api/transfer'
   },
 
   subject: {
-    host: 'http://3.137.213.20:8085/api/subject'
+    host: 'http://localhost:8080/api/subject'
   },
 
   course: {
-    host: 'http://3.137.213.20:8085/api/course',
-    host_image: 'http://3.137.213.20:8085/api/course/upload/image'
+    host: 'http://localhost:8080/api/course',
+    host_image: 'http://localhost:8080/api/course/upload/image'
   },
 
   grupalCourse: {
-    host: 'http://3.137.213.20:8085/api/grupal/course',
-    host_image: 'http://3.137.213.20:8085/api/grupal/course/image'
+    host: 'http://localhost:8080/api/grupal/course',
+    host_image: 'http://localhost:8080/api/grupal/course/image'
   },
 
   feedBack: {
-    host: 'http://3.137.213.20:8085/api/feed-back',
+    host: 'http://localhost:8080/api/feed-back',
   },
 
   retirements: {
-    host: 'http://3.137.213.20:8085/api/retirement',
+    host: 'http://localhost:8080/api/retirement',
   },
 
   jwt: {
-    host: 'http://3.137.213.20:8085/oauth/token'
+    host: 'http://localhost:8080/oauth/token'
   },
 
   appCredentials: {
